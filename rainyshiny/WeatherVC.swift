6d64c7212c377e03a7b3a1236283293efa886125
Davis Samuel <dsamuel5733@gmail.com>
2017-12-22 00:13:30 -0500

    Added basic UI

---
+++ rainyshiny/WeatherVC.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherVC.swift
+//  rainyshiny
+//
+//  Created by Davis Samuel on 12/21/17.
+//  Copyright © 2017 Davis Samuel. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
