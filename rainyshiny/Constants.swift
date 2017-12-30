3955747982085099b4416bb3b38ccbdb7b6ff241
Davis Samuel <dsamuel5733@gmail.com>
2017-12-29 23:13:51 -0500

    Added constants file and current wethr file

---
+++ rainyshiny/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  rainyshiny
+//
+//  Created by Davis Samuel on 12/29/17.
+//  Copyright © 2017 Davis Samuel. All rights reserved.
+//
+
+import Foundation
+
+let BASE_URL = "http://samples.openweathermap.org/data/2.5/weather?"
+let LATITUDE = "lat="
+let LONGITUDE = "&lon="
+let APP_ID = "&appid="
+let API_KEY = "754f3f1e174904b1b3740a4cbd55b71d"
+
+let CURRENT_WEATHER_URL = "\(BASE_URL)\(LATITUDE)-36\(LONGITUDE)123\(APP_ID)\(API_KEY)"
+
+
